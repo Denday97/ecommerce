dd427780f7f7a6f29b1894d78b6aced6b8dec9f6
Denday97 <eugenierony@gmail.com>
2023-02-17 15:43:19 +0100

    pagination

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Categories;
+use App\Repository\ProductsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,9 +13,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class CategoriesController extends AbstractController
 {
     #[Route('/{slug}', name: 'list')]
-    public function list(Categories $category): Response
+    public function list(Categories $category, ProductsRepository $productsRepository, Request $request): Response
     {
-        $products = $category->getProducts();
+        // on va chercher le numéro de la page
+        $page = $request->query->getInt('page', 1);
+
+        $products = $productsRepository->findProductsPaginated($page, $category->getSlug(), 2);
+
 
         return $this->render('categories/list.html.twig', compact('category', 'products'));
         // return $this->render('categories/list.html.twig', [
